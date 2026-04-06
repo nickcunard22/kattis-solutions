915dfe4e2dde9db28b8cc0be45a0fa714b13871a
nrcunard <nrcunard@ncsu.edu>
2026-04-06 18:41:04 -0400

    im gophing it

--- gopher_2/gopher_2.py
+++ gopher_2/gopher_2.py
@@ -1,11 +1,40 @@
 from sys import stdin
+from math import dist
 
-n, m, s, v = map(int, input().split())
+while True:
+    try:
+        n, m, s, v = map(int, input().split())
+    except EOFError:
+        break
 
-# gophers
-gophers = [tuple(map(float, input().split())) for _ in range(n)]
+    gophers = [tuple(map(float, input().split())) for _ in range(n)]
 
-# holes
-holes = [tuple(map(float, input().split())) for _ in range(m)]
+    holes = [tuple(map(float, input().split())) for _ in range(m)]
 
-print(gophers, holes)
+    graph = [[] for _ in range(n)] # maps gopher -> hole
+
+    for i, gopher in enumerate(gophers):
+        for j, hole in enumerate(holes):
+            if dist(gopher, hole) < v * s: # if goph can make it
+                graph[i].append(j) # add new edge from i -> j
+
+    # hungarian
+    def dfs(u, visited, matching):
+        ''' brute force dfs, return true if augmenting path exists '''
+        for v in graph[u]:
+            if not visited[v]:
+                visited[v] = True
+            
+                # if v is unmatched, or future neighbor is unmatched
+                if matching[v] == -1 or dfs(matching[v], visited, matching):
+                    matching[v] = u
+                    return True
+                
+        return False
+
+    matching = [-1] * m # hole -> gopher
+
+    for i in range(n):
+        dfs(i, [False] * m, matching)
+
+    print(matching.count(-1))
